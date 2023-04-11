658de626bcd3d9fd5c795bfccee382b5faa3fa9a
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-04-11 16:02:47 +0300

    ASCII art image of a cat

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+//I am going to create and populate print statements until every line of ASCII image is accounted for. I started with a draft of a multiline comment
+    println("           *     *")
+    println("          * * _ * *")
+    println("          (  O  O  )")
+    println("          ==( Y )==")
+    println("           ---U---")
+    println("    *        *")
+    println("      *  *  *  ")
+}
